9754f8cf9165e8e390044a626810955ad7a1ae1a
Avni Sharma <avni.sharma262000@gmail.com>
2023-01-31 08:18:48 +0100

    initial Commit

---
+++ routes/blog.routes.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const Blog = require('../models/Blog.model');
+
+// @desc    Get all blogs by user id
+// @route   '/api/blogs
+// @access  Private
+
+router.get('/blogs',(req,res) => 
+{
+    
+        Blog.find({ user: req.payload._id })
+            .then(foundblogs => res.json(foundblogs))
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+    
+})
+
+//  POST /api/blogs  -  Creates a new blog
+router.post('/blogs', (req, res, next) => {
+    const { title, content } = req.body;
+   
+    Blog.create({ title, content, user:req.payload._id })
+      .then(response => res.json(response))
+      .catch(err => res.json(err));
+  });
+  
+
+  router.put("/blogs/:blogId", (req, res)=>{
+    
+    const { title, content } = req.body;
+    
+    Blog.findOneAndUpdate({ _id: req.params.id, user: req.payload._id }, { title, content }, { new: true })
+            .then(updatedBlog => res.json(updatedBlog))
+            .catch(err => console.log(err))
+
+})
+
+router.delete("/blogs/:BlogId", (req, res)=>{
+    /* const { BlogId } = req.params; */
+    
+    Blog.findOneAndDelete({ _id: req.params.id, user: req.payload._id })
+            .then(deletedBlog => res.json(deletedBlog))
+            .catch(err => console.log(err))
+})
+
+router.get("/blogs/:blogId", (req, res) => {
+    /* const { blogId } = req.params; */
+
+    Blog.findOne({ _id: req.params.id, user: req.payload._id })
+            .populate('tasks')
+            .then(foundProject => res.json(foundProject))
+            .catch(err => console.log(err))
+})
+module.exports = router;
